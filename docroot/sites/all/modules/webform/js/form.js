76b935246f4fce5d7d308d2b1f17ae61390ba215
Mike Alexeenko <mike.alexeenko@blinkreaction.com>
2014-07-30 16:43:12 +0300

    Adding custom js files to page and webform

---
+++ docroot/sites/all/modules/webform/js/form.js
@@ -0,0 +1,10 @@
+(function (aa)
+{
+  aa(document).ready(function ()
+  {
+    alert('form.js loaded for the Webform -> contact form');
+  })
+})(jQuery);
+
+
+
---
+++ docroot/sites/all/themes/batcopy/js/property.js
@@ -0,0 +1,10 @@
+(function (aa)
+{
+  aa(document).ready(function ()
+  {
+    alert('property.js loads on the property page');
+  })
+})(jQuery);
+
+
+
